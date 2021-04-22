5c95ec1735d6db9c535adf1622b5233e8388febd
GeorgeKalash <george.kalash@softmachine.co>
2021-04-22 09:53:27 +0300

    first-push

---
+++ ArgusCR1012/Sales/Templates/SalesQuotation.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace ArgusCR1012.Sales.Templates
+{
+    partial class SalesQuotation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.ReportFooter = new DevExpress.XtraReports.UI.ReportFooterBand();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // ReportHeader
+            // 
+            this.ReportHeader.Name = "ReportHeader";
+            // 
+            // Detail
+            // 
+            this.Detail.Name = "Detail";
+            // 
+            // ReportFooter
+            // 
+            this.ReportFooter.Name = "ReportFooter";
+            // 
+            // SalesQuotation
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.ReportHeader,
+            this.Detail,
+            this.ReportFooter});
+            this.Version = "20.1";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.ReportHeaderBand ReportHeader;
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.ReportFooterBand ReportFooter;
+    }
+}
---
+++ ArgusCR1012/Sales/Templates/SalesQuotation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace ArgusCR1012.Sales.Templates
+{
+    public partial class SalesQuotation : ArgusRPT.BaseReport
+    {
+        public SalesQuotation()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
