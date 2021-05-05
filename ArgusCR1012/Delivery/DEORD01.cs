53cdb2a284a2cf1ad64a1a5fd242e106442a8463
GeorgeKalash <george.kalash@softmachine.co>
2021-05-05 21:15:43 +0300

    DEORD01

---
+++ ArgusCR1012/Delivery/DEORD01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace ArgusCR1012.Delivery
+{
+    public partial class DEORD01 : Reports.BaseReport
+    {
+        public DEORD01()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
