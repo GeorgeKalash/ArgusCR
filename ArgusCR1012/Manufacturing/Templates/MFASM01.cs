cd751a1076ec6096dce2d7eb2332b5e7047f817d
GeorgeKalash <george.kalash@softmachine.co>
2021-06-22 11:47:36 +0300

    MF102

--- ArgusCR1012/Manufacturing/Templates/MFASM01.cs
+++ ArgusCR1012/Manufacturing/Templates/MFASM01.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Printing;
 using DevExpress.XtraReports.UI;
 
 //public class Assembly : Activity
@@ -46,6 +47,59 @@ namespace ArgusCR1012.Manufacturing.Templates
         public MFASM01()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<ArgusDS.Manufacturing.Reports.AssemblyPrintView> webObject = deserializeGet<ArgusDS.Manufacturing.Reports.AssemblyPrintView>();
+
+            //if (webObject)
+
+            DataSource = webObject.record.items;
+
+            //clientName_data.Text = webObject.record.header.clientName;
+            //reference_data.Text = webObject.record.header.reference;
+            //date_data.Text = webObject.record.header.date.ToString(sessionInfo.dateFormat);
+
+            //billAddressName_data.Text = webObject.record.billAddress?.name;
+            //billAddressPhone_data.Text = webObject.record.billAddress?.phone;
+
+            //subtotal_data.Text = webObject.record.header.subtotal.ToString("N2");
+            //vatAmount_data.Text = webObject.record.header.vatAmount.ToString("N2");
+            //amount_data.Text = webObject.record.header.amount.ToString("N2");
+
+            //spName_data.Text = webObject.record.header.spName;
+            //spCellPhone_data.Text = webObject.record.header.spCellPhone;
+
+            //if (webObject.record.header.deliveryDate != null)
+            //    deliveryDate_data.Text = ((DateTime)webObject.record.header.deliveryDate).ToString(sessionInfo.dateFormat);
+
+            //amountInWords_data.Text = SharedClasses.NumberToWords.multiLingualNumberInText((decimal)webObject.record.header.amount, 2, Convert.ToInt16(sessionInfo.languageId));
+
+            //if (webObject.record.header.expiryDate != null)
+            //    validity_data.Text = string.Format("{0} days: {1}", webObject.record.header.validity, ((DateTime)webObject.record.header.expiryDate).ToString(sessionInfo.dateFormat));
+
+            //if (webObject.record.shipAddress != null)
+            //{
+            //    shipToAddress_data.Text = string.Format("{0}\\r\\n{1}\\r\\n{2}", webObject.record.shipAddress.street1, webObject.record.shipAddress.street2, webObject.record.shipAddress.city);
+            //}
+
+            base.OnBeforePrint(e);
+        }
+
+        protected override void OnDataSourceRowChanged(DataSourceRowEventArgs e)
+        {
+            //lineNo.Text = itemsCounter.ToString();
+            //++itemsCounter;
+            base.OnDataSourceRowChanged(e);
+        }
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "Custom\\R1012\\MF102";
         }
 
     }
