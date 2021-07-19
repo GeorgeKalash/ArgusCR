a70b0cbb16a48911ea75349a065a7c2cf2f47b35
Aline Haji Manoukian <aline.manoukian@softmachine.co>
2021-07-19 14:33:28 +0300

    Update MFBOM01

--- ArgusCR1012/Manufacturing/Templates/MFBOM01.cs
+++ ArgusCR1012/Manufacturing/Templates/MFBOM01.cs
@@ -32,13 +32,16 @@ namespace ArgusCR1012.Manufacturing.Templates
             reference_data.Text = webObject.record.header.reference;
             name_data.Text = webObject.record.header.name;
             type_data.Text = webObject.record.header.type.ToString();
-            description_data.Text = webObject.record.header.description;
+            sku_data.Text = webObject.record.header.description;
+            itemName_data.Text = webObject.record.header.itemName;
 
             status_data.Text = webObject.record.header.activeStatus.ToString();
             version_data.Text = webObject.record.header.version;
             date_data.Text = ((DateTime)webObject.record.header.date).ToString(sessionInfo.dateFormat);
+            description_data.Text = webObject.record.header.description;
             bomQty_data.Text = webObject.record.header.qty.ToString();
-            
+
+         
             printSignature_lbl.Text = reportSignature();
             base.OnBeforePrint(e);
         }
@@ -58,23 +61,29 @@ namespace ArgusCR1012.Manufacturing.Templates
             bomTitle_lbl.Text = labelText(2);
 
             billOfMaterial_lbl.Text = labelText(3);
+
             reference_lbl.Text = labelText(4);
             name_lbl.Text = labelText(5);
             type_lbl.Text = labelText(6);
-            description_lbl.Text = labelText(7);
+            sku_lbl.Text = labelText(7);
+            itemName_lbl.Text = labelText(8);
+           
 
-            status_lbl.Text = labelText(8);
-            version_lbl.Text = labelText(9);
-            date_lbl.Text = labelText(10);
-            bomQty_lbl.Text = labelText(11);
+            status_lbl.Text = labelText(9);
+            version_lbl.Text = labelText(10);
+            date_lbl.Text = labelText(11);
+            description_lbl.Text = labelText(12);
+            bomQty_lbl.Text = labelText(13);
 
-            lineNo_lbl.Text = labelText(12);
-            sku_lbl.Text = labelText(13);
-            itemName_lbl.Text = labelText(14);
-            qty_lbl.Text = labelText(15);
-
-            printSignature_lbl.Text = labelText(16);
-            pageNumber_lbl.Text = labelText(17);
+            lineNo_lbl.Text = labelText(14);
+            skuComponent_lbl.Text = labelText(15);
+            itemNameComponent_lbl.Text = labelText(16);
+            qty_lbl.Text = labelText(17);
+            percentage_lbl.Text = labelText(18);
+            siteName_lbl.Text = labelText(19);
+        
+            printSignature_lbl.Text = labelText(20);
+            pageNumber_lbl.Text = labelText(21);
         }
 
         protected override string dictionaryStore()
