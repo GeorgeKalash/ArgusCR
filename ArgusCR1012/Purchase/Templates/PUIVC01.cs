a37ab4738f024a1d2d0982dc64f3ae0b06b26850
GeorgeKalash <George.Kalash@softmachine.co>
2021-09-03 16:17:58 +0300

    PU

---
+++ ArgusCR1012/Purchase/Templates/PUIVC01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace ArgusCR1012.Purchase.Templates
+{
+    public partial class PUIVC01 : Reports.BaseReport
+    {
+        public PUIVC01()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ ArgusCR1012/Purchase/Templates/PUORD01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace ArgusCR1012.Purchase.Templates
+{
+    public partial class PUORD01 : Reports.BaseReport
+    {
+        public PUORD01()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
