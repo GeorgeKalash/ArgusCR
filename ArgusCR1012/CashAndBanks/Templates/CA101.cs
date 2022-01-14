80e5fad158b59e3c9bad0f45f98da3d608f9a1a2
Aline Haji Manoukian <aline.manoukian@softmachine.co>
2022-01-14 16:40:39 +0200

    Update

--- ArgusCR1012/CashAndBanks/Templates/CA101.cs
+++ ArgusCR1012/CashAndBanks/Templates/CA101.cs
@@ -3,15 +3,36 @@ using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Printing;
 
 namespace ArgusCR1012.CashAndBanks.Templates
 {
-    public partial class CA101 : DevExpress.XtraReports.UI.XtraReport
+    public partial class CA101 : Reports.BaseReport
     {
         public CA101()
         {
             InitializeComponent();
         }
 
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<ArgusDS.Cash.Reports.CA101> webObject = deserializeGet<ArgusDS.Cash.Reports.CA101>();
+            DataSource = webObject.record;
+
+
+            base.OnBeforePrint(e);
+
+        }
+
+        protected override void labelsText()
+        {
+          
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "Custom\\R1012\\CA101";
+        }
     }
 }
+
