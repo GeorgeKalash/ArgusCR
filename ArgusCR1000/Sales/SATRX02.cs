85e4920f0b69f4f6ea3b4e7bc41791755848f2ed
Aline Haji Manoukian <aline.manoukian@softmachine.co>
2022-02-28 08:35:31 +0200

    ADD SATRX02

---
+++ ArgusCR1000/Sales/SATRX02.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing.Printing;
+using ArgusDS.Sales.Reports;
+
+namespace ArgusCR1000.Sales.Templates
+{
+    public partial class SATRX02 : Reports.BaseReport
+    {
+        public SATRX02()
+        {
+            InitializeComponent();            
+        }
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            DataSource = CreateDataSourceFromText();
+            base.OnBeforePrint(e);
+        }
+
+        private void Detail_BeforePrint(object sender, PrintEventArgs e)
+        {
+        }
+    }
+}
