f96ce10970b2936b13b81fc1d87bf5fce47b844c
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2022-11-14 12:32:08 +0200

    Add ArgusCR1028/ SA103

---
+++ ArgusCR1028/Sales/SA103.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+using DevExpress.XtraReports.UI;
+using ArgusDS.Sales.Reports;
+using ArgusDS.Inventory;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArgusCR1028.Sales.SA103
+
+{
+    public partial class SA103 : ArgusRPT.BaseReport
+    {
+  
+      public SA103()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+          RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
+          RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+
+            SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
+            DataSource = webObject.record.items;
+
+            logo_data.ImageUrl = webObject.record.companyInfo.logoUrl;
+            companyName_data.Text = webObject.record.companyInfo.name;
+            companyVatNo_data.Text = webObject.record.companyInfo.taxNo;
+            addressName_data.Text = webObject.record.companyInfo.address?.name;
+            addressStreet_data.Text = webObject.record.companyInfo.address?.street1;
+            addressMobile_data.Text = webObject.record.companyInfo.address?.phone;
+            licenseNo_data.Text = webObject.record.companyInfo.licenseNo;
+
+            reference_data.Text = webObject.record.trxHeader.reference;
+
+            if (webObject.record.logTime != null)
+            {
+                date_data.Text = ((DateTime)webObject.record.logTime).ToString(sessionInfo.dateFormat) + " " + ((DateTime)webObject.record.logTime).ToString("HH:mm");
+            }
+
+            plant_data.Text = webObject.record.trxHeader.plantName;
+            address_data.Text = webObject.record.companyInfo.address?.street1;
+            spName_data.Text = webObject.record.salesPerson?.name;
+            cellPhone_data.Text = webObject.record.salesPerson?.cellPhone;
+
+            clientRef_data.Text = webObject.record.client.reference;
+            clientName_data.Text = webObject.record.client.name;
+            clientVATNo_data.Text = webObject.record.client?.vatNumber;
+            phoneNumber_data.Text = webObject.record.billAddress?.phone;
+            cAddress_data.Text = webObject.record.billAddress?.street1;
+          
+            subtotal_data.Text = webObject.record.trxHeader.subtotal.ToString("N2");
+            tdAmount_data.Text = webObject.record.trxHeader.tdAmount.ToString();
+
+            double TotalAmountNonSubjectToTax = webObject.record.trxHeader.subtotal - (webObject.record.trxHeader.tdAmount ?? 0);
+            totalSub_data.Text = TotalAmountNonSubjectToTax.ToString("N2");
+
+            vatAmount_data.Text = webObject.record.trxHeader.vatAmount.ToString("N2");
+            amount_data.Text = webObject.record.trxHeader.amount.ToString("N2");
+
+            if (webObject.record.currencyProfileId != null)
+                amountInWords_data.Text = SharedClasses.NumberToWords.multiLingualNumberInText((decimal)webObject.record.trxHeader.amount, webObject.record.currencyProfileId, 2);
+
+            description_data.Text = webObject.record.trxHeader.description;
+
+            clientName2_data.Text = webObject.record.trxHeader.clientName;
+            spName2_data.Text = webObject.record.trxHeader.spName;
+
+            QRCode.Text = new KSAeInvoiceQrCode(webObject.record.companyInfo.name, webObject.record.companyInfo.taxNo, (DateTime)webObject.record.logTime, webObject.record.trxHeader.amount.ToString(), webObject.record.trxHeader.vatAmount.ToString()).ToBase64();
+
+            base.OnBeforePrint(e);
+        }
+
+        protected override void OnDataSourceRowChanged(DataSourceRowEventArgs e)
+        {
+            ArgusDS.Sales.ItemView obj = ((List<ArgusDS.Sales.ItemView>)DataSource)[e.CurrentRow];
+
+            double netUnitPrice = obj.unitPrice - (obj.mdValue ?? 0);
+            double epWithVAT = obj.extendedPrice + obj.vatAmount;
+
+            unitPrice_data.Text = netUnitPrice.ToString("N2");
+
+            extendedPriceWithVAT_data.Text = epWithVAT.ToString("N2");
+
+            base.OnDataSourceRowChanged(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+        protected override string dictionaryStore()
+        {
+            return "CR1028.SA103";
+        }
+    }
+}
+
