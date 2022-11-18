46a98e969309e134ba50bfce2224c25a12d0d7a2
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2022-11-18 12:17:21 +0200

    ArgusCR1028: Delete SA103/ADD SAIVC01

--- ArgusCR1028/Sales/SAIVC01.cs
+++ ArgusCR1028/Sales/SAIVC01.cs
@@ -10,31 +10,27 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace ArgusCR1028.Sales
-
 {
-    public partial class SA103 : ArgusRPT.BaseReport
+    public partial class SAIVC01 : ArgusRPT.BaseReport
     {
   
-      public SA103()
+      public SAIVC01()
         {
             InitializeComponent();
         }
 
         protected override void OnBeforePrint(PrintEventArgs e)
         {
-          RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
-          RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+            RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
+            RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+
+            const int BPIDC_VAT = 2;
 
             SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
             DataSource = webObject.record.items;
 
-            logo_data.ImageUrl = webObject.record.companyInfo.logoUrl;
             companyName_data.Text = webObject.record.companyInfo.name;
-            companyVatNo_data.Text = webObject.record.companyInfo.taxNo;
-            addressName_data.Text = webObject.record.companyInfo.address?.name;
-            addressStreet_data.Text = webObject.record.companyInfo.address?.street1;
-            addressMobile_data.Text = webObject.record.companyInfo.address?.phone;
-            licenseNo_data.Text = webObject.record.companyInfo.licenseNo;
+            taxNo_data.Text = webObject.record.companyInfo.taxNo;
 
             reference_data.Text = webObject.record.trxHeader.reference;
 
@@ -44,59 +40,64 @@ namespace ArgusCR1028.Sales
             }
 
             plant_data.Text = webObject.record.trxHeader.plantName;
+            licenseNo_data.Text = webObject.record.companyInfo.licenseNo;
+            crNo_data.Text = webObject.record.companyInfo.crNo;
             address_data.Text = webObject.record.companyInfo.address?.street1;
-            spName_data.Text = webObject.record.salesPerson?.name;
-            cellPhone_data.Text = webObject.record.salesPerson?.cellPhone;
 
             clientRef_data.Text = webObject.record.client.reference;
-            clientName_data.Text = webObject.record.client.name;
-            clientVATNo_data.Text = webObject.record.client?.vatNumber;
-            phoneNumber_data.Text = webObject.record.billAddress?.phone;
+            clientName_data.Text = webObject.record.trxHeader.clientName;
+
+            if (webObject.record.masterIDs != null)
+            {
+                ArgusDS.BusinessPartners.MasterIDNumberView vatID = webObject.record.masterIDs.FirstOrDefault(x => x.incId == BPIDC_VAT);
+                if (vatID != null)
+                    clientVATNo_data.Text = vatID.idNum;
+            }
+
+            if (webObject.record.bpDefaultIDCategoryId != null)
+            {
+                ArgusDS.BusinessPartners.MasterIDNumberView otherID = webObject.record.masterIDs.FirstOrDefault(x => x.incId == webObject.record.bpDefaultIDCategoryId);
+                if (otherID != null)
+                {
+                    idNo_data.Text = otherID.idNum;
+                    idNo_lbl.Text = otherID.incName;
+                }
+            }
+
             cAddress_data.Text = webObject.record.billAddress?.street1;
           
             subtotal_data.Text = webObject.record.trxHeader.subtotal.ToString("N2");
-            tdAmount_data.Text = webObject.record.trxHeader.tdAmount.ToString();
-
-            double TotalAmountNonSubjectToTax = webObject.record.trxHeader.subtotal - (webObject.record.trxHeader.tdAmount ?? 0);
-            totalSub_data.Text = TotalAmountNonSubjectToTax.ToString("N2");
-
             vatAmount_data.Text = webObject.record.trxHeader.vatAmount.ToString("N2");
             amount_data.Text = webObject.record.trxHeader.amount.ToString("N2");
+            amountInWords_data.Text = SharedClasses.NumberToWords.multiLingualNumberInText((decimal)webObject.record.trxHeader.amount, 2, 2);
 
-            if (webObject.record.currencyProfileId != null)
-                amountInWords_data.Text = SharedClasses.NumberToWords.multiLingualNumberInText((decimal)webObject.record.trxHeader.amount, webObject.record.currencyProfileId, 2);
-
-            description_data.Text = webObject.record.trxHeader.description;
 
             clientName2_data.Text = webObject.record.trxHeader.clientName;
             spName2_data.Text = webObject.record.trxHeader.spName;
 
             QRCode.Text = new KSAeInvoiceQrCode(webObject.record.companyInfo.name, webObject.record.companyInfo.taxNo, (DateTime)webObject.record.logTime, webObject.record.trxHeader.amount.ToString(), webObject.record.trxHeader.vatAmount.ToString()).ToBase64();
-
             base.OnBeforePrint(e);
         }
 
         protected override void OnDataSourceRowChanged(DataSourceRowEventArgs e)
         {
             ArgusDS.Sales.ItemView obj = ((List<ArgusDS.Sales.ItemView>)DataSource)[e.CurrentRow];
-
+            
             double netUnitPrice = obj.unitPrice - (obj.mdValue ?? 0);
             double epWithVAT = obj.extendedPrice + obj.vatAmount;
 
             unitPrice_data.Text = netUnitPrice.ToString("N2");
-
             extendedPriceWithVAT_data.Text = epWithVAT.ToString("N2");
-
             base.OnDataSourceRowChanged(e);
         }
 
         protected override void labelsText()
         {
         }
+
         protected override string dictionaryStore()
         {
             return "CR1028.SA103";
         }
     }
 }
-
