ce0a0b22558bb01d57a6ff9b854190122e6d1901
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2022-12-07 08:13:52 +0200

    ADD SA103

---
+++ ArgusCR120/Sales/SA103/MetalSubReports.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+using DevExpress.XtraReports.UI;
+using ArgusDS.Sales.Reports;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArgusCR120.Sales.SA103
+{
+    public partial class MetalSubReports : ArgusRPT.BaseReport
+    {
+        public List<TrxPrintView> data;
+
+        public MetalSubReports()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+
+        {
+            RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
+            RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+
+            DataSource = data;
+            base.OnBeforePrint(e);
+        }
+
+        private void MetalSubReports_DataSourceRowChanged(object sender, DataSourceRowEventArgs e)
+        {
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR120.SA103";
+        }
+    }
+}
