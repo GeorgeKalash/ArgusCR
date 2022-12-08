548bb6253019087e67692c5fb41bbd8185dc9c03
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2022-12-08 12:07:17 +0200

    ADD ArgusCR1029

--- ArgusCR1029/Properties/AssemblyInfo.cs
+++ ArgusCR1029/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArgusCR120")]
+[assembly: AssemblyTitle("ArgusCR1029")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArgusCR120")]
+[assembly: AssemblyProduct("ArgusCR1029")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
