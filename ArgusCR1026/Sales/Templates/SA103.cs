7f2d7aa444c5d8072ee040a7612814f5daa8a727
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-01-18 09:52:54 +0200

    Add ArgusCR1026

---
+++ ArgusCR1026/Sales/Templates/SA103.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace ArgusCR1026.Sales.Templates
+{
+    public partial class SA103 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public SA103()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
