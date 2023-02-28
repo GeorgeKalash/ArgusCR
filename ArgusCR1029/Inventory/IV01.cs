9ae5e3dccf0ae92868053c63142ce0b48c723e70
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2023-02-28 12:20:01 +0200

    ItemList

---
+++ ArgusCR1029/Inventory/IV01.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace ArgusCR1029.Inventory
+{
+    public partial class IV01 : ArgusRPT.BaseReport
+    {
+        public IV01()
+        {
+            InitializeComponent();
+        }
+
+        protected override string reportName()
+        {
+            return title_lbl.Text;
+        }
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            //SharedClasses.JsonProtocol.QryStructure<ArgusDS.Inventory.Reports.> obj = deserializeList< ArgusDS.Inventory.Reports.> ();
+            //DataSource = obj.list;
+
+            printSignature.Text = reportSignature();
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+            title_lbl.Text = labelText(0);
+            sku_lbl.Text = labelText(1);
+            itemName_lbl.Text = labelText(2);
+            categoryRef_lbl.Text = labelText(3);
+            groupRef_lbl.Text = labelText(4);
+            lineRef_lbl.Text = labelText(5);
+            msRef_lbl.Text = labelText(6);
+            stdCost_lbl.Text = labelText(7);
+            weight_data.Text = labelText(8);
+            salePrice_lbl.Text = labelText(9);
+            vatAmount_lbl.Text = labelText(10);
+            priceGroupRef_lbl.Text = labelText(11);
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1029.IV01";
+        }
+    }
+}
