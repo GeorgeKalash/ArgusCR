88c00ae6a6e225131869b72794718d59dba01c77
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-03-28 12:38:13 +0300

    Update TransferSubReports.Designer.cs

--- ArgusCR1029/Inventory/IV102/TransferSubReports.Designer.cs
+++ ArgusCR1029/Inventory/IV102/TransferSubReports.Designer.cs
@@ -560,7 +560,7 @@ namespace ArgusCR1029.Inventory.IV102
             this.xrTableCell24.BackColor = System.Drawing.Color.WhiteSmoke;
             this.xrTableCell24.Borders = ((DevExpress.XtraPrinting.BorderSide)((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Bottom)));
             this.xrTableCell24.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[salesPrice]*[qty]")});
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "sumSum([salesPrice]*[qty])")});
             this.xrTableCell24.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold);
             this.xrTableCell24.Multiline = true;
             this.xrTableCell24.Name = "xrTableCell24";
