4c689fa8c42229e6c54cd7b76647dfec0daf000b
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2023-04-03 13:21:48 +0300

    IV426

---
+++ ArgusCR1029/Inventory/IV426.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace ArgusCR1029.Inventory
+{
+    public partial class IV426 : ArgusRPT.BaseReport
+    {
+        public IV426()
+        {
+            InitializeComponent();
+        }
+        protected override string reportName()
+        {
+            return title_lbl.Text;
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.QryStructure<Custom.CR1029.IV426> webObject = deserializeList<Custom.CR1029.IV426>();
+            DataSource = webObject.list;
+
+            fiscalYear_param.Text = Parameters.Count > 0 ? Parameters[0].Value.ToString() : string.Empty;
+            startDate_param.Text = Parameters.Count > 1 ? Parameters[1].Value.ToString() : string.Empty;
+            endDate_param.Text = Parameters.Count > 2 ? Parameters[2].Value.ToString() : string.Empty;
+
+            printSignature.Text = reportSignature();
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+        protected override string dictionaryStore()
+        {
+            return "CR1029.IV426";
+        }
+    }
+}
