8434d207ec69c10f7195056c44145d25afea89ac
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-04-04 11:16:42 +0300

    ADD CR1034

---
+++ ArgusCR1034/POS/Templates/PS101GLD/InvoiceReceiptSubReports.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace ArgusCR1034.POS.Templates.PS101GLD
+{
+    public partial class InvoiceReceiptSubReports : ArgusRPT.BaseReport
+    {
+        public List<ArgusDS.PointOfSale.InvoiceReceiptView> data;
+
+        public InvoiceReceiptSubReports()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            DataSource = data;
+            base.OnBeforePrint(e);
+        }
+
+        private void InvoiceReceiptSubReports_DataSourceRowChanged(object sender, DataSourceRowEventArgs e)
+        {
+
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1034.PS101";
+        }
+    }
+}
