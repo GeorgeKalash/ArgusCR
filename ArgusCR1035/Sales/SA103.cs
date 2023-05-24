6ecb1f8d25913d43e834f29f2bc52cbd778a4074
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-05-24 09:03:58 +0300

    Add ArgusCR1035

---
+++ ArgusCR1035/Sales/SA103.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+using DevExpress.XtraReports.UI;
+using ArgusDS.Sales.Reports;
+using ArgusDS.Inventory;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArgusCR1035.Sales
+{
+    public partial class SA103 : ArgusRPT.BaseReport
+    {
+        public SA103()
+        {
+            InitializeComponent();
+        }
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
+            DataSource = webObject.record.items;
+            
+            base.OnBeforePrint(e);
+        }
+
+
+        protected override void labelsText()
+        {
+        }
+        protected override string dictionaryStore()
+        {
+            return "";
+        }
+    }
+}
+
