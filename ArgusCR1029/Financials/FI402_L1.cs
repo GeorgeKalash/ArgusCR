2d488906b313a7261cbfcd12bda41fc80516a3bc
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-06-14 13:00:07 +0300

    updates

---
+++ ArgusCR1029/Financials/FI402_L1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using ArgusDS.Financials;
+using DevExpress.XtraReports.UI;
+
+namespace ArgusCR1029.Financials
+{
+    public partial class FI402_L1 : ArgusRPT.BaseReport
+    {
+        public FI402_L1()
+        {
+            InitializeComponent();
+        }
+
+        protected override string reportName()
+        {
+            return reportTitle_lbl.Text;
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.QryStructure<Custom.CR1029.FI402_L1> obj = deserializeList<Custom.CR1029.FI402_L1>();
+            DataSource = obj.list;
+
+            fiscalYear_param.Text = Parameters.Count > 0 ? Parameters[0].Value.ToString() : string.Empty;
+            startDate_param.Text = Parameters.Count > 1 ? Parameters[1].Value.ToString() : string.Empty;
+            endDate_param.Text = Parameters.Count > 2 ? Parameters[2].Value.ToString() : string.Empty;
+            accountGroup_param.Text = Parameters.Count > 4 ? Parameters[4].Value.ToString() : string.Empty;
+
+            logo_data.ImageUrl = companyInfo.logoUrl;
+            printSignature.Text = reportSignature();
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1029.FI402_L1";
+        }
+    }
+}
