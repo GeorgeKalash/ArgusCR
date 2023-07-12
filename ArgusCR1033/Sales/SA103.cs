d453badbcc0ac600410ef4492c1bba4adaf8b680
Aline Haji Manoukian <86043180+alinehajimanoukian@users.noreply.github.com>
2023-07-12 11:49:49 +0300

    ADD CR1033

---
+++ ArgusCR1033/Sales/SA103.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Printing;
+using ArgusDS.Sales.Reports;
+
+namespace ArgusCR1033.Sales
+{
+    public partial class SA103 : ArgusRPT.BaseReport
+    {
+        public SA103()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
+            DataSource = webObject.record.items;
+
+            companyName_data.Text = webObject.record.companyInfo.name;
+            clientName_data.Text = webObject.record.trxHeader.clientName;
+            docRef_data.Text = webObject.record.trxHeader.reference;
+            subtotal_data.Text = webObject.record.trxHeader.subtotal.ToString("N2");
+            netPrice_data.Text = webObject.record.trxHeader.amount.ToString("N2");
+
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1033.SA103";
+        }
+    }
+}
