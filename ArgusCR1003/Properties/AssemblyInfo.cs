1454ea1c088118dc96440598a09960c87def575f
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2024-04-19 09:35:36 +0300

    ADD CR1003

--- ArgusCR1003/Properties/AssemblyInfo.cs
+++ ArgusCR1003/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArgusCR1004")]
+[assembly: AssemblyTitle("ArgusCR1003")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArgusCR1004")]
+[assembly: AssemblyProduct("ArgusCR1003")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
