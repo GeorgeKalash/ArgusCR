ff0954373d573426290d3c48495e4b8beb7aa025
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2024-10-09 09:03:07 +0300

    Update IV102.cs

--- ArgusCR1029/Inventory/IV102/IV102.cs
+++ ArgusCR1029/Inventory/IV102/IV102.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.ComponentModel;
-using System.Drawing;
 using System.Drawing.Printing;
 using DevExpress.XtraReports.UI;
-using ArgusDS.Sales.Reports;
 using System.Collections.Generic;
 
 namespace ArgusCR1029.Inventory.IV102
