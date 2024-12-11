ddce6b7b163c861cd6b8d8d58fae20a621517f08
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2024-12-11 09:42:37 +0200

    add CR1010

---
+++ ArgusCR1010/Sales/SA51303.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing.Printing;
+using ArgusDS.Sales.Reports;
+
+namespace ArgusCR1010.Sales
+{
+    public partial class SA51303 : ArgusRPT.BaseReport
+    {
+        public SA51303()
+        {
+            InitializeComponent();
+        }
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
+            DataSource = webObject.record.items;
+
+            clientName_data.Text = webObject.record.trxHeader.clientName;
+            clientVATNo_data.Text = webObject.record.client.vatNumber;
+            cAddress_data.Text = webObject.record.billAddress?.street1;
+
+            reference_data.Text = webObject.record.trxHeader.reference;
+            date_data.Text = webObject.record.trxHeader.date.ToString(sessionInfo.dateFormat);
+
+            base.OnBeforePrint(e);
+        }
+        protected override void labelsText()
+        {
+        }
+        protected override string dictionaryStore()
+        {
+            return "CR1010.SA103";
+        }
+    }
+}
