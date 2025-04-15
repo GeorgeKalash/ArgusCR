361aafdfa589e43990c5f84119bdd8c522706820
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2025-04-15 14:40:22 +0300

    Update SA51303.cs

--- ArgusCR1010/Sales/SA51303.cs
+++ ArgusCR1010/Sales/SA51303.cs
@@ -42,8 +42,10 @@ namespace ArgusCR1010.Sales
             vatAmount_data.Text = webObject.record.trxHeader.vatAmount.ToString("N0");
             amount_data.Text = webObject.record.trxHeader.amount.ToString("N0");
 
-            decimal Airsi = webObject.record.trxHeader.amount * 0.05m; 
-            airsi_data.Text = Airsi.ToString("N0");
+            decimal Airsi = webObject.record.trxHeader.amount * 0.05m;
+            // airsi_data.Text = Airsi.ToString("N0");
+
+            airsi_data.Text = webObject.record.trxHeader.miscAmount.ToString();
 
             decimal TotalAPayer = webObject.record.trxHeader.amount + (webObject.record.trxHeader.amount * 0.05m);
             totalAPayer_data.Text = TotalAPayer.ToString("N0");
