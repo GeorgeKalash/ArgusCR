2e0f67fee09901e217e741fa2a6433e3c2f6f9b6
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2025-06-27 10:19:52 +0300

    ADD SA103

---
+++ ArgusCR1039/Sales/SA103/MetalSubReports.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Printing;
+
+namespace ArgusCR1039.Sales.SA103
+{
+    public partial class MetalSubReports : ArgusRPT.BaseReport
+    {
+        public List<ArgusDS.Logistics.MetalSummary> data;
+
+        public MetalSubReports()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+
+        {
+            RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
+            RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+
+            DataSource = data;
+            base.OnBeforePrint(e);
+        }
+
+        private void MetalSubReports_DataSourceRowChanged(object sender, DataSourceRowEventArgs e)
+        {
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1029.SA103";
+        }
+    }
+}
---
+++ ArgusCR1039/Sales/SA103/SA103.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing.Printing;
+using DevExpress.XtraReports.UI;
+using ArgusDS.Sales.Reports;
+using System.Collections.Generic;
+
+namespace ArgusCR1039.Sales.SA103
+{
+    public partial class SA103 : ArgusRPT.BaseReport
+    {
+        public SA103()
+        {
+            InitializeComponent();
+        }
+
+        public override void setSessionInfo(Dictionary<string, string> _reportHeaders)
+        {
+            base.setSessionInfo(_reportHeaders);
+            ((ArgusRPT.BaseReport)(InvoiceItemsSubReports.ReportSource)).setSessionInfo(sessionInfo);
+            ((ArgusRPT.BaseReport)(MetalSubReports.ReportSource)).setSessionInfo(sessionInfo);
+            initSubReports();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            RightToLeft = DevExpress.XtraReports.UI.RightToLeft.No;
+            RightToLeftLayout = DevExpress.XtraReports.UI.RightToLeftLayout.No;
+
+            SharedClasses.JsonProtocol.GetStructure<TrxPrintView> webObject = deserializeGet<TrxPrintView>();
+
+            reference_data.Text = webObject.record.trxHeader.reference;
+            date_data.Text = webObject.record.trxHeader.date.ToString(sessionInfo.dateFormat);
+            plant_data.Text = webObject.record.trxHeader.plantName;
+            spName_data.Text = webObject.record.salesPerson?.name;
+            address_data.Text = companyInfo.address?.street1;
+
+            clientRef_data.Text = webObject.record.client.reference;
+            clientName_data.Text = webObject.record.trxHeader.clientName;
+            clientVATNo_data.Text = webObject.record.trxHeader.clientVATNo;
+            phoneNumber_data.Text = webObject.record.billAddress?.phone;
+            cAddress_data.Text = webObject.record.billAddress?.street1;
+            exhibitionManager_data.Text = webObject.record.trxHeader.contactName;
+
+            tdPct_data.Text = webObject.record.trxHeader.tdPct.ToString();
+            subtotal_data.Text = webObject.record.trxHeader.subtotal.ToString("N2");
+            tdAmount_data.Text = webObject.record.trxHeader.tdAmount.ToString();
+            vatAmount_data.Text = webObject.record.trxHeader.vatAmount.ToString("N2");
+            amount_data.Text = webObject.record.trxHeader.amount.ToString("N2");
+            amountInWords_data.Text = SharedClasses.NumberToWords.multiLingualNumberInText((decimal)webObject.record.trxHeader.amount, 10, 2);
+
+            description_data.Text = webObject.record.trxHeader.description;
+
+            spName2_data.Text = webObject.record.salesPerson?.name;
+
+            ((InvoiceItemsSubReports)(InvoiceItemsSubReports.ReportSource)).data = webObject.record.items;
+            ((MetalSubReports)(MetalSubReports.ReportSource)).data = webObject.record.metalSummaries;
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+
+        protected override string dictionaryStore()
+        {
+            return "CR1029.SA103";
+        }
+    }
+}
