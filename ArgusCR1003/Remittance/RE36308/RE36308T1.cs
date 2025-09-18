7c9868d0f3d415983fee49b30f7a5015417acf3d
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2025-09-18 11:25:19 +0300

    ADD RE36308T1

---
+++ ArgusCR1003/Remittance/RE36308/RE36308T1.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace ArgusCR1003.Remittance.RE36308
+{
+    public partial class RE36308T1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public RE36308T1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
