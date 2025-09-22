75c4614f3c3b07e6bd31447fc87c52d2316b84a3
Aline Haji Manoukian <Aline.Manoukian@softmachine.co>
2025-09-22 11:55:44 +0300

    ADD RE36308T2

---
+++ ArgusCR1003/Remittance/RE36308/RE36308T2.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing.Printing;
+
+namespace ArgusCR1003.Remittance.RE36308
+{
+    public partial class RE36308T2 : ArgusRPT.BaseReport
+    {
+        public RE36308T2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforePrint(PrintEventArgs e)
+        {
+            SharedClasses.JsonProtocol.GetStructure<ArgusCT_DS.Remittance.Report.RT105> webObject = deserializeGet<ArgusCT_DS.Remittance.Report.RT105>();
+
+ 
+
+            base.OnBeforePrint(e);
+        }
+
+        protected override void labelsText()
+        {
+        }
+        protected override string dictionaryStore()
+        {
+            return "CR1003.RE36308T2";
+        }
+
+    }
+}
